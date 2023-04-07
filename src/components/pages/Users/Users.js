31e540147708a46b6a9b76986fa6d673d2d31478
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-04-07 21:49:13 +0600

    Ascending and filtering complete

--- src/components/pages/Users/Users.js
+++ src/components/pages/Users/Users.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDeleteUserMutation, useGetUsersQuery } from '../../../features/users/usersSlice';
 import User from '../User/User';
 import Loader from '../../shared/Loader';
@@ -6,8 +6,14 @@ import { toast } from 'react-hot-toast';
 
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
     const { data, isLoading, isError, error } = useGetUsersQuery(null, { refetchOnMountOrArgChange: true });
     const [deleteUser, { isSuccess }] = useDeleteUserMutation();
+
+
+    useEffect(() => {
+        setUsers(data?.data)
+    }, [data?.data])
 
     if (isLoading) {
         return <div className='h-screen flex justify-center items-center'>
@@ -21,11 +27,40 @@ const Users = () => {
         </div>
     }
 
-    let users = data?.data;
+
 
     const handleSearch = key => {
-        const filtered = users?.filter(user => user.name.includes(key))
+        console.log(key);
+        const filtered = users?.filter((user) => {
 
+            const userName = (user.name).toLowerCase();
+            const lowerCaseKey = key.toLowerCase();
+
+            return userName.includes(lowerCaseKey);
+        })
+
+        if (key === "") {
+            setUsers(data?.data)
+        }
+        setUsers(filtered)
+
+    }
+
+    const handleAscending = value => {
+        if (value === "all") {
+            setUsers(data.data)
+        }
+        else if (value === "byMobileNumber") {
+            const usersArray = [...users];
+            const ascendingByNumber = usersArray?.sort((a, b) => a.mobileNumber - b.mobileNumber);
+            setUsers(ascendingByNumber);
+        }
+        else if (value === "byName") {
+            const usersArray = [...users];
+            const ascendingByName = usersArray.sort((a, b) => a.name.localeCompare(b.name));
+            console.log(ascendingByName);
+            setUsers(ascendingByName);
+        }
     }
 
 
@@ -38,14 +73,29 @@ const Users = () => {
 
     return (
         <div className='max-w-7xl mx-auto'>
-            <input
-                onChange={(e) => handleSearch(e.target.value)}
-                type='text'
-                id='search'
-                name='search'
-                className='w-full p-2 focus:outline-primary bg-gray-light'
-                required={true}
-            />
+            <div className='flex lg:flex-row md:flex-row flex-col justify-between px-3 mt-4 gap-y-4'>
+                <input
+                    onChange={(e) => handleSearch(e.target.value)}
+                    type='text'
+                    id='search'
+                    name='search'
+                    className='p-2 focus:outline-primary bg-gray-light'
+                    required={true}
+                    placeholder='Search By Name'
+                />
+                <div>
+                    <select
+                        onChange={(e) => handleAscending(e.target.value)}
+                        className='p-2 focus:outline-primary bg-gray-light w-full'
+                        name="ascending"
+                        id=""
+                    >
+                        <option value="all">All</option>
+                        <option value="byName">Ascending By Name</option>
+                        <option value="byMobileNumber">Ascending By Mobile Number</option>
+                    </select>
+                </div>
+            </div>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 p-3'>
                 {
                     users?.map((user, index) =>
